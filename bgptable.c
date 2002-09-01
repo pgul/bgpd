25170e98124bcb1814ee40d20e2bc2684b680939
Pavel Gulchouck <gul@gul.kiev.ua>
2002-09-01 08:30:47 +0000

    logging improved

--- bgptable.c
+++ bgptable.c
@@ -145,9 +145,10 @@ static void perlinitmap(void)
    LEAVE;
    if (SvTRUE(ERRSV))
    {
-     Log(0, "Perl eval error: %s\n", SvPV(ERRSV, n_a));
+     Log(0, "Perl %s() eval error: %s\n", plinitmap, SvPV(ERRSV, n_a));
      exit(4);
-   }
+   } else
+     Log(2, "Perl %s() called\n", plinitmap);
 }
 
 void perlbgpup(void)
@@ -166,9 +167,10 @@ void perlbgpup(void)
    LEAVE;
    if (SvTRUE(ERRSV))
    {
-     Log(0, "Perl eval error: %s\n", SvPV(ERRSV, n_a));
+     Log(0, "Perl %s() eval error: %s\n", plbgpup, SvPV(ERRSV, n_a));
      exit(4);
-   }
+   } else
+     Log(2, "Perl %s() called\n", plbgpup);
 }
 
 static class_type perlsetclass(char *community, char *aspath, char *prefix)
@@ -201,7 +203,7 @@ static class_type perlsetclass(char *community, char *aspath, char *prefix)
    LEAVE;
    if (SvTRUE(ERRSV))
    {
-     Log(0, "Perl eval error: %s\n", SvPV(ERRSV, n_a));
+     Log(0, "Perl %s() eval error: %s\n", plsetclass, SvPV(ERRSV, n_a));
      exit(4);
    }
    if (n_a == 0 && prc)
